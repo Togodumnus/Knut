a15bb6c95af1fc77d90ff8bc09c9becf66ea7dc4
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-03-19 15:35:57 +0100

    Add Action definition

---
+++ src/shell/Action.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdbool.h>
+
+struct Action {
+
+    /**
+     * cmd
+     *
+     * Une commande à exécuter, peut-être une action
+     *
+     * @example "rm /bin"                   Une commande
+     * @example "(echo hello && ls -l)"     Une autre action
+     */
+    char *cmd;
+
+    /**
+     * andBefore
+     *
+     * Vaut true si l'action était précédée d'un ET logique &&
+     */
+    bool andBefore;
+
+    /**
+     * orBefore
+     *
+     * Vaut true si l'action était précédée d'un OU logique ||
+     */
+    bool orBefore;
+
+}
+
+typedef Action Action;
+
