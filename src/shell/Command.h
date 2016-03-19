2147f7bea1b0630ec9954d6214c33d916779fa0a
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-03-19 15:40:03 +0100

    Add Command definition

---
+++ src/shell/Command.h
@@ -0,0 +1,33 @@
+#pragma once
+
+/**
+ * CommandType
+ */
+enum CommandType {
+    EXECUTABLE, //la commande est un path vers un exécutable, ex: "./bin/libs/ls"
+    LIBRARY,    //la commande est un nom de libraire Knut, ex "ls"
+    ACTION      //la commande est une action, ex: "echo hello && ls -l"
+};
+
+typedef CommandType CommandType;
+
+
+struct Command {
+
+    CommandType type;
+
+    /**
+     * cmd
+     *
+     * La commande à lancer :
+     * - le path vers l'éxécutable si type == EXECUTABLE
+     * - le nom de la library si type == LIBRARY
+     * - la chaîne a analyser si type == ACTION
+     */
+    char *cmd;
+
+    int argc,
+    char *argv[];
+};
+
+typedef Command Command;
