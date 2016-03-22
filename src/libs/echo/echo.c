79679af671f207f6ae6c2353c4c2b0a6e4c2dcac
Tom MARRUCCI <tom.marrucci@etu.univ-nantes.fr>
2016-03-22 17:27:05 +0100

    Echo avec option n et en propre

--- src/libs/echo/echo.c
+++ src/libs/echo/echo.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
+int kecho(int argc, char * const argv[]) {
+	int opt = 0;
+	int opt_n = 0; // pour savoir si on a mis l'option -n
 	if (argc<2) { 
 		printf("\n");
 		return 0;
 	}
-	printf("%s\n",argv[1]);
+	char c;
+	while((c = getopt(argc, argv, "ne")) != -1) {
+		opt++;
+		switch(c) {
+			case 'e':
+				printf("e\n");
+				break;
+			case 'n': 
+				opt_n = 1;
+				break;
+			case '?': // option pas reconnu
+				exit(-1);
+		}
+	}
+	int i;
+	for (i=opt+1;i<argc;i++) {
+		printf("%s ", argv[i]);
+	}
+	if (!opt_n) printf("\n");
 	return 0;
 }
---
+++ src/libs/echo/main.c
@@ -0,0 +1,6 @@
+#include "echo.h"
+
+int main(int argc, char * const argv[])
+{
+	return kecho(argc,argv);
+}
