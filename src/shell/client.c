6041170f8620a2356c668fc1a6db9cc4d2ce9996
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-03-29 07:09:35 +0200

    Fix include for linux

--- src/shell/client.c
+++ src/shell/client.c
@@ -1,6 +1,9 @@
+#ifndef __APPLE__
+    #define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
