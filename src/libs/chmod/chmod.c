5063875495d70af931b7b734aef5ac51c13524e1
Quentin Billaud <quentin.billaud@etu.univ-nantes.fr>
2016-03-31 18:45:29 +0200

    Chmod qui marche avec les droits en version numérique

---
+++ src/libs/chmod/chmod.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+
+#include "../../LIB.h"
+
+int octal_decimal(int n) /* Function to convert octal to decimal */
+{
+    int decimal=0, i=0, rem;
+    while (n!=0)
+    {
+        rem = n%10;
+        n/=10;
+        decimal += rem*pow(8,i);
+        ++i;
+    }
+    return decimal;
+}
+
+int chmodLib(int argc, char *argv[]) {
+    if (argc>1)
+    {
+        int droit = atoi(argv[1]);
+        droit = octal_decimal(droit);
+        int resultat = chmod(argv[2],(mode_t)droit);
+        if (resultat==-1)
+           {
+               printf("Erreur\n");
+               return -1;
+           }  
+    }
+    return 0;
+}
+
+/**
+ * Init
+ *
+ * S'enregistre dans le shell dans le cas d'un chargement de la librairie
+ * dynamique
+ */
+void Init(EnregisterCommande enregisterCommande) {
+    enregisterCommande("chmod", chmodLib);
+}
