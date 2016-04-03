292272861df2e1d0ed21b79c4569b44c7ddc97f9
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-03 12:47:18 +0200

    Fix cp file_to_file

--- src/libs/cp/cp.c
+++ src/libs/cp/cp.c
@@ -8,16 +8,43 @@
 #include <errno.h>
 #include <libgen.h>
 
+const int BUFFER_SIZE = 1024;
+
+/**
+ * kcp_file_to_file
+ *
+ * @param  {char *}     path1   La source
+ * @param  {char *}     path1   La destination
+ */
 int kcp_file_to_file(const char *path1, const char *path2) {
+
 	FILE * f1 = fopen(path1, "r");
 	FILE * f2 = fopen(path2, "w");
 
-	char * line;
- 	size_t len = 0;
-	ssize_t read;
+    if (f1 == NULL) {
+        printf("Can't open %s\n", path1);
+        exit(EXIT_FAILURE);
+    } else if (f2 == NULL) {
+        printf("Can't open %s\n", path2);
+        exit(EXIT_FAILURE);
+    }
 
- 	while ((read = getline(&line, &len, f1)) != -1) {
-       fprintf(f2,"%s",line);
+    //la copie
+    int n;
+    char buf[BUFFER_SIZE];
+    while ((n = read(fileno(f1), buf, BUFFER_SIZE)) > 0 ) {
+        int nw = 0;
+        do {
+            nw = write(fileno(f2), buf + nw, n - nw);
+            if (nw == -1) {
+                printf("Write error\n");
+                break;
+            }
+        } while (nw != n);
+    }
+
+    if (n == 1) {
+        printf("Read error\n");
     }
 
   	fclose(f1);
@@ -27,19 +54,19 @@ int kcp_file_to_file(const char *path1, const char *path2) {
 
 int kcp_file_to_dir(char *file_name, char *dir_path) {
 	if (dir_path[strlen(dir_path)-1]!='/') {
-		strcat(dir_path, "/"); 
+		strcat(dir_path, "/");
 	} // au cas ou l'utilisateur à entré le chemin sans / à la fin (/home/user au lieu de /home/user/)
 
-	char dir_pathFull[strlen(dir_path)]; // Besoin d'une 2eme variable car sinon dir_path change 
+	char dir_pathFull[strlen(dir_path)]; // Besoin d'une 2eme variable car sinon dir_path change
 
 	printf("file %s copy in %s\n", basename(file_name), dir_path);
 
-	strcpy(dir_pathFull, dir_path); 
+	strcpy(dir_pathFull, dir_path);
 	strcat(dir_pathFull,  basename(file_name)); // dir_pathFull devient le chemin du fichier
-	
+
 	fclose(fopen(dir_pathFull, "w")); // on créé le fichier puis on ferme le FILE
 	kcp_file_to_file(file_name, dir_pathFull); // puis on copie le fichier dans la nouveau fichier créé
-	memset (dir_pathFull, 0, sizeof (dir_pathFull)); // on vide le chemin du fichier 
+	memset (dir_pathFull, 0, sizeof (dir_pathFull)); // on vide le chemin du fichier
 	return 0;
 }
 
@@ -71,26 +98,26 @@ int kcp_dir_to_dir(char *dir_path_src, char *dir_path_dest) {
 	}
 	else
 		path_to_copy = dir_path_src;
-	
-	// Pour créé les repertoire qui seront copié 
+
+	// Pour créé les repertoire qui seront copié
 	char * make_dir = malloc(sizeof (path_to_copy) + sizeof(dir_path_dest));
 	strcat(make_dir, dir_path_dest);
 	strcat(make_dir, path_to_copy);
-	mkdir(make_dir, 0775); 
+	mkdir(make_dir, 0775);
 	printf("make %s\n",make_dir );
 
 	while ((dptr_src=readdir(dirp_src))) { // on lit le dossier source
 		if ((strcmp(dptr_src->d_name,"..")!=0)&&(strcmp(dptr_src->d_name,".")!=0)) { // on ne tiens pas compte de . et ..
- 
+
 			char path[strlen(dir_path_src)]; // pour avoir le path des fichiers du dossier
 
-			if (path_to_copy[strlen(dir_path_src)-1]=='/'){ 
+			if (path_to_copy[strlen(dir_path_src)-1]=='/'){
 				sprintf(path,"%s%s",dir_path_src,dptr_src->d_name);
 			}
-			else {		
+			else {
 				sprintf(path,"%s/%s",dir_path_src,dptr_src->d_name);
 			}
-			
+
 			printf("%s\n",path );
 			if (lstat(path, &st)==-1) {
 				printf("erreur stat\n");
@@ -110,7 +137,7 @@ int kcp_dir_to_dir(char *dir_path_src, char *dir_path_dest) {
 	}
 	free(make_dir);
 	closedir(dirp_src);
-	
+
 
 	/*
 	* Ouvrir le dossier src
@@ -118,7 +145,7 @@ int kcp_dir_to_dir(char *dir_path_src, char *dir_path_dest) {
 	* parcours les fichiers
 		* Si fichier : kcp_file_to_dir(dir_path_src+nameFile, dir_path_dest)
 		* Si dossier : appeler méthode recursif : kcp_dir_to_dir(dir_path_src, dir_path_dest)
-	* 
+	*
 	*/
 	return 0;
 }
@@ -134,13 +161,13 @@ int kcp(int argc, char * const argv[]) {
 			case 'r':
 				printf("r\n");
 				break;
-			case '?': // option pas reconnu
+			case '?': //option no reconnue
 				exit(-1);
 		}
 	}
 	// TODO : vérifier que c'est sous la forme "kcp f1 f2 f3 dir1"
 	// cf myls avec stat pour voir si c un dir
-	return kcp_dirs_to_dir(argc, argv);
+    return kcp_dirs_to_dir(argc, argv);
 }
 
 int main(int argc, char * const argv[]) {
