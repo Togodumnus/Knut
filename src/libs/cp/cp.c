58ebd6cdbc34701937ae3010414ffdb9b69f44f6
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-04 12:32:36 +0200

    Try to fix cp

--- src/libs/cp/cp.c
+++ src/libs/cp/cp.c
@@ -8,6 +8,8 @@
 #include <errno.h>
 #include <libgen.h>
 
+int indent = 0;
+
 const int BUFFER_SIZE = 1024;
 
 const int FOLDER_RIGHT = 0775;
@@ -17,7 +19,7 @@ const int FOLDER_RIGHT = 0775;
  *
  * @param  {char *} path        Le dossier à créer
  */
-void mkdirFull(const char *path) {
+void mkdirFull(char *path) {
     struct stat st;
 
     if (stat(path, &st) == -1) {
@@ -29,32 +31,6 @@ void mkdirFull(const char *path) {
 }
 
 /**
- * appendToStr
- *
- * Ajout de ch à la fin de str
- *
- * @param  {char *}     str     La chaîne à compléter
- * @param  {char}       ch      Le caractère à ajouter
- *
- * @return {char *}             Un pointeur vers une nouvelle chaîne
- */
-char *appendToStr(const char *str, char ch) {
-    int l = strlen(str);
-
-    char *newStr = (char *) malloc((l + 2) * sizeof(char));
-    strcpy(newStr, str);
-    if (newStr == NULL) {
-        perror("Malloc error");
-        exit(EXIT_FAILURE);
-    }
-
-    newStr[l] = ch;
-    newStr[l+1] = '\0';
-
-    return newStr;
-}
-
-/**
  * kcp_file_to_file
  *
  * Copie le contenu d'un fichier dans un autre
@@ -62,12 +38,10 @@ char *appendToStr(const char *str, char ch) {
  * @param  {char *}     path1   La source
  * @param  {char *}     path2   La destination
  */
-int kcp_file_to_file(const char *path1, const char *path2) {
+int kcp_file_to_file(char *path1, char *path2) {
 
     FILE * f1 = fopen(path1, "r");
     FILE * f2 = fopen(path2, "w");
-
-    printf("kcp_file_to_file : %s -> %s\n", path1, path2);
 
     if (f1 == NULL) {
         printf("Can't open %s\n", path1);
@@ -78,10 +52,10 @@ int kcp_file_to_file(const char *path1, const char *path2) {
     }
 
     //la copie
-    int n;
+    ssize_t n;
     char buf[BUFFER_SIZE];
     while ((n = read(fileno(f1), buf, BUFFER_SIZE)) > 0 ) {
-        int nw = 0;
+        ssize_t nw = 0;
         do {
             nw = write(fileno(f2), buf + nw, n - nw);
             if (nw == -1) {
@@ -108,20 +82,17 @@ int kcp_file_to_file(const char *path1, const char *path2) {
  * @param  {char *}     file_path   La source
  * @param  {char *}     dir_path   La destination
  */
-int kcp_file_to_dir(char *file_path, const char *dir_path) {
+int kcp_file_to_dir(char *file_path, char *dir_path) {
     FILE * f;
 
-    printf("kcp_file_to_dir : %s -> %s\n", file_path, dir_path);
-
     // au cas ou l'utilisateur a entré le chemin sans / à la fin
-    char *dir = appendToStr(dir_path, '/');
+    char *dir = dir_path;
 
     //On crée le path vers le fichier destination
     char *fileName = basename(file_path);
     char file_pathFull[strlen(dir) + strlen(fileName)];
     strcpy(file_pathFull, dir);
     strcpy(file_pathFull + strlen(dir), fileName);
-    free(dir);
 
     if ((f = fopen(file_pathFull, "w")) == NULL) { // création du fichier
         printf("Can't open %s\n", file_pathFull);
@@ -140,7 +111,7 @@ int kcp_file_to_dir(char *file_path, const char *dir_path) {
  * Copie plusieurs fichiers en entrée dans un répertoire
  *
  * @param  {int *}        argc   Le nombre d'argument
- * @param  {char const*}  argv   Les arguments
+ * @param  {char *}  argv   Les arguments
  */
 int kcp_files_to_dir(int argc, char * argv[]) {
     struct stat st;
@@ -170,69 +141,68 @@ int kcp_files_to_dir(int argc, char * argv[]) {
  * @param  {char *}  dir_path_src    Le chemin du répertoire à copier
  * @param  {char *}  dir_path_dest   Le chemin du répertoire de destination
  */
-int kcp_dir_to_dir(char *dir_path_src, const char *dir_path_dest) {
-    DIR *dirp_src;
+int kcp_dir_to_dir(char *dir_path_src, char *dir_path_dest) {
+
+    DIR *dirp_src = (DIR *) malloc(sizeof(DIR));
+    if (dirp_src == NULL) {
+        perror("Malloc error");
+        exit(1);
+    }
     struct dirent *dptr_src;
-    struct stat st;
 
-    char *dirName;
+    //ajout d'un / à la destination
+    char dir_path_dest_tmp[strlen(dir_path_dest) + 2];
+    strcpy(dir_path_dest_tmp, dir_path_dest);
+    strcat(dir_path_dest_tmp, "/");
+    mkdirFull(dir_path_dest_tmp);
 
-    // on ouvre le dossier sourc
-    if ((dirp_src = opendir(dir_path_src)) == NULL) {
-        printf("Can't open directory %s\n", dir_path_src);
+    //ajout d'un / à la source
+    char dir_path_src_tmp[strlen(dir_path_src) + 100];
+    strcpy(dir_path_src_tmp, dir_path_src);
+    strcat(dir_path_src_tmp, "/");
+
+    if ((dirp_src = opendir(dir_path_src_tmp)) == NULL) {
+        printf("Can't open directory %s %s\n", dir_path_src_tmp, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    //on ajoute un / à la fin du path si nécessaire
-    char *dirDest = appendToStr(dir_path_dest, '/');
-    mkdirFull(dirDest);
+    while ((dptr_src = readdir(dirp_src)) != NULL) { //tous les éléments de la source
 
-    //extraction du nom du dossier
-    dirName = basename(dir_path_src);
-
-    //Création du sous dossier dans la destination
-    char destDir[strlen(dirName) + strlen(dirDest)];
-    strcpy(destDir, dirDest);
-    strcpy(destDir + strlen(dirDest), dirName);
-    mkdirFull(destDir);
-
-    printf("DESTDIR : %s\n", destDir);
-
-    while ((dptr_src = readdir(dirp_src))) { //tous les éléments de la source
-
-        // on ne tiens pas compte de . et ..
         if ((strcmp(dptr_src->d_name, "..") != 0)
                 && (strcmp(dptr_src->d_name,".") != 0)) {
+            struct stat st;
 
-            int pathLength = strlen(dir_path_src)
-                             + strlen(dptr_src->d_name) + 1;
-            char path[pathLength];
-
-            //construction du path vers l'élement d'origine
-            if (dirName[strlen(dir_path_src)-1] == '/'){
-                sprintf(path,"%s%s", dir_path_src, dptr_src->d_name);
-            } else {
-                sprintf(path,"%s/%s", dir_path_src, dptr_src->d_name);
-            }
-            printf("-> %p %s\n", &destDir, destDir);
+            char path[strlen(dir_path_src_tmp) + strlen(dptr_src->d_name) + 2];
+            strcpy(path, dir_path_src);
+            strcat(path, "/");
+            strcat(path, dptr_src->d_name);
 
             //distinction fichiers et dossiers
             if (stat(path, &st) == -1) {
-                printf("path=%s\n", path); //TODO
                 printf("error stat\n");
                 exit(EXIT_FAILURE);
             }
-            if (S_ISDIR(st.st_mode)) { // repertoire
-                printf("Directory\n");
+            if (S_ISDIR(st.st_mode)) { //répertoire
+                //construction de la destination
+                char destDir[strlen(dir_path_dest_tmp) + strlen(dptr_src->d_name) + 2];
+                strcpy(destDir, dir_path_dest_tmp);
+                strcat(destDir, dptr_src->d_name);
+                strcat(destDir, "/");
+                mkdirFull(dir_path_dest_tmp);
+
+                indent++;
                 kcp_dir_to_dir(path, destDir);
-            } else { // fichier
-                /*kcp_file_to_dir(path, destDir);*/
+                indent--;
+            } else { //fichier
+                kcp_file_to_dir(path, dir_path_dest_tmp);
             }
         }
     }
 
-    free(dirDest);
-    closedir(dirp_src);
+    if(closedir(dirp_src) != 0) {
+        perror("Closedir error");
+        exit(1);
+    }
 
     return 0;
 }
@@ -243,7 +213,7 @@ int kcp_dir_to_dir(char *dir_path_src, const char *dir_path_dest) {
  * Copie plusieurs répertoires en entrée dans un repertoire
  *
  * @param  {int *}        argc   Le nombre d'argument
- * @param  {char const*}  argv   Les arguments
+ * @param  {char *}  argv   Les arguments
  */
 int kcp_dirs_to_dir(int argc, char *argv[]) {
     int i;
