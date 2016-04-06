1e7da19d185af8a521ffb7dfa72a325732d7fec6
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-06 07:26:05 +0200

    Add pwd usage

--- src/libs/pwd/pwd.c
+++ src/libs/pwd/pwd.c
@@ -2,8 +2,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
 
-int kpwd(int argc, char const *argv[]) {
+int usage() {
+
+    printf("\
+Knut pwd \n\
+\n\
+usage: pwd\n\
+");
+
+    return 0;
+}
+
+int kpwd(int argc, char *argv[]) {
+    int opt = 0;
+
+    while ((opt = getopt(argc, argv, "")) != -1) {
+        switch (opt) {
+            default:
+                return usage();
+                break;
+        }
+    }
+
 	char *pwd = NULL;
     size_t n = 0;
 	pwd = (char *) getcwd(pwd, n);
