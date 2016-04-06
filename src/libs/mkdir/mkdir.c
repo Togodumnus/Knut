cf947e696c3bf8ebe67b8d52106e853bbcc334f5
Tom MARRUCCI <tom.marrucci@etu.univ-nantes.fr>
2016-04-06 19:46:27 +0200

    Commentaire propre + option p (pas encore fonctionnel)

--- src/libs/mkdir/mkdir.c
+++ src/libs/mkdir/mkdir.c
@@ -4,11 +4,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
+#include <libgen.h>
+#include <string.h>
 
-
-/*
-* Convertir octal en décimal
-*/
+/**
+ * octal_decimal
+ *
+ * Convertie un nombre decimal en base octal 
+ *
+ * @param  {int}  n    Le nombre à convertir
+ */
 int octal_decimal(int n)
 {
     int decimal=0, i=0, rem;
@@ -23,49 +28,99 @@ int octal_decimal(int n)
 }
 
 /**
-* Mode verbeux
-* Affiche un message avec le nom du repertoire créé
-*/
+ * kmkdir_v
+ *
+ * Option 'v' de mkdir : affiche un message  
+ *
+ * @param  {char *}  dirname    Le nom du répertoire créé
+ */
 int kmkdir_v(char * dirname) {
 	printf("mkdir: created directory %s\n", dirname);
 	return 0;
 }
 
 /**
-* Ajout des permissions
-* Exemple : mkdir -m 777 dir
-* Créé un répértoire dir avec toutes les permissions pour tout le monde
-*/
-int kmkdir_m(int argc, char * const argv[]) {
+ * kmkdir_m
+ *
+ * Option 'm' de mkdir : droit préciser lors de la création du répertoire 
+ *
+ * @param  {int}  	   argc    Le nombre d'arguments d'entrée
+ * @param  {char[] *}  argv    Les arguments d'entrée
+ */
+int kmkdir_m(int argc, char * argv[]) {
 	if (argc<4) {
 		printf("kmkdir : missing operand\n");
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 	return octal_decimal(atoi(argv[argc-2]));	
 }
 
-/*
-* Pour traiter les options
-*/
-int kmkdir(int argc, char * const argv[]) {
+/**
+ * kmkdir_p
+ *
+ * Option 'p' de mkdir : créer les répertoires parents nécessaire
+ *
+ * @param  {char *}   path    		 Le chemin du répertoire à créé
+ * @param  {int}  	  permissions    Les permissions du répertoire créé
+ */
+int kmkdir_p(char * path, int permissions) {
+	if (!strcmp(dirname(path),".")) { // si path est "seul" (usr/) (création du premier dossier)
+		if (mkdir(path, permissions) == -1) {
+			perror("kmkdir p");
+			exit(EXIT_FAILURE);
+		}
+		return 0;
+	}
+	else { // sinon on rappel kmkdir_p jusqu'à créé le premier dossier
+		kmkdir_p(path, permissions);
+	}
+	// pour les autres dossiers
+	if (mkdir(path, permissions) == -1) {
+			perror("kmkdir p2");
+			exit(EXIT_FAILURE);
+	}
+	return 0;	
+}
+
+/**
+ * kmkdir
+ *
+ * Fonction principale pour traiter les options
+ *
+ * @param  {int}  	   argc    Le nombre d'arguments d'entrée
+ * @param  {char[] *}  argv    Les arguments d'entrée
+ */
+int kmkdir(int argc, char * argv[]) {
 	if (argc<2) {
 		printf("kmkdir : missing operand\n");
-		return -1;
+		exit(EXIT_FAILURE);
 	}
 	int c;
+	int vFlag = 0;
 	int permissions = 0775; // permissions de base
-	while((c = getopt(argc, argv, "mv")) != -1) {
+	while((c = getopt(argc, argv, "pmv")) != -1) {
 		switch(c) {
 			case 'm':
 				permissions = kmkdir_m(argc, argv);
 				break;
 			case 'v': 
-				kmkdir_v(argv[argc-1]);
+				vFlag = 1;
 				break;
+			case 'p':
+				kmkdir_p(argv[argc-1], permissions);
 			case '?': // option pas reconnu
-				exit(-1);	
+				exit(EXIT_FAILURE);	
 		}
 	}
-	mkdir(argv[argc-1], permissions);
+	if (mkdir(argv[argc-1], permissions) == -1) {
+		perror("kmkdir");
+		exit(EXIT_FAILURE);
+	}
+	if (vFlag) 
+		kmkdir_v(argv[argc-1]);
 	return 0;
+}
+
+int main(int argc, char * argv[]) {
+	return kmkdir(argc, argv);
 }
--- src/libs/mkdir/mkdir.h
+++ src/libs/mkdir/mkdir.h
@@ -4,6 +4,8 @@ int octal_decimal(int n);
 
 int kmkdir_v(char * dirname);
 
-int kmkdir_m(int argc, char * const argv[]);
+int kmkdir_m(int argc, char * argv[]);
 
-int kmkdir(int argc, char * const argv[]);
+int kmkdir_p(char * path, int permissions);
+
+int kmkdir(int argc, char * argv[]);
