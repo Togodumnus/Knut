8c61f60430ed97a190aa77920c146a44323e67d3
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-06 13:47:35 +0200

    Fix tab to spaces

--- src/libs/echo/echo.c
+++ src/libs/echo/echo.c
@@ -5,31 +5,31 @@
 #include "../../LIB.h"
 
 int kecho(int argc, char * argv[]) {
-	int opt = 0;
-	int opt_n = 0; // pour savoir si on a mis l'option -n
-	if (argc<2) {
-		printf("\n");
-		return 0;
-	}
-	char c;
-	while((c = getopt(argc, argv, "ne")) != -1) {
-		opt++;
-		switch(c) {
-			case 'e': // Fait de base
-				break;
-			case 'n':
-				opt_n = 1;
-				break;
-			case '?': // option pas reconnu
-				exit(-1);
-		}
-	}
-	int i;
-	for (i=opt+1;i<argc;i++) {
-		printf("%s ", argv[i]);
-	}
-	if (!opt_n) printf("\n");
-	return 0;
+    int opt = 0;
+    int opt_n = 0; // pour savoir si on a mis l'option -n
+    if (argc < 2) {
+        printf("\n");
+        return 0;
+    }
+    char c;
+    while((c = getopt(argc, argv, "ne")) != -1) {
+        opt++;
+        switch(c) {
+            case 'e': // Fait de base
+                break;
+            case 'n':
+                opt_n = 1;
+                break;
+            case '?': // option pas reconnu
+                exit(-1);
+        }
+    }
+    int i;
+    for (i=opt+1;i<argc;i++) {
+        printf("%s ", argv[i]);
+    }
+    if (!opt_n) printf("\n");
+    return 0;
 }
 
 /**
