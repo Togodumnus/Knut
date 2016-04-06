2603e683b28d1fac496f32f77cfa8cbfc113cc96
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-06 13:16:31 +0200

    Fix for linux

--- src/libs/mv/mv.c
+++ src/libs/mv/mv.c
@@ -1,10 +1,10 @@
+#define _XOPEN_SOURCE 500
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <string.h>
 #include <libgen.h>
 
 #include "../../LIB.h"
