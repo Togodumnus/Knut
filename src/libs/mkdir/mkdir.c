d1a751a542073610ccacda18d9e657d9ff00516e
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-06 13:31:32 +0200

    Add Makefile

--- src/libs/mkdir/mkdir.c
+++ src/libs/mkdir/mkdir.c
@@ -1,16 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <math.h>
 
+#include "../../LIB.h"
 
 /*
 * Convertir octal en décimal
 */
-int octal_decimal(int n)
-{
+int octal_decimal(int n) {
     int decimal=0, i=0, rem;
     while (n!=0)
     {
@@ -27,8 +27,8 @@ int octal_decimal(int n)
 * Affiche un message avec le nom du repertoire créé
 */
 int kmkdir_v(char * dirname) {
-	printf("mkdir: created directory %s\n", dirname);
-	return 0;
+    printf("mkdir: created directory %s\n", dirname);
+    return 0;
 }
 
 /**
@@ -37,35 +37,45 @@ int kmkdir_v(char * dirname) {
 * Créé un répértoire dir avec toutes les permissions pour tout le monde
 */
 int kmkdir_m(int argc, char * const argv[]) {
-	if (argc<4) {
-		printf("kmkdir : missing operand\n");
-		return -1;
-	}
-	return octal_decimal(atoi(argv[argc-2]));	
+    if (argc<4) {
+        printf("kmkdir : missing operand\n");
+        return -1;
+    }
+    return octal_decimal(atoi(argv[argc-2]));
 }
 
 /*
 * Pour traiter les options
 */
-int kmkdir(int argc, char * const argv[]) {
-	if (argc<2) {
-		printf("kmkdir : missing operand\n");
-		return -1;
-	}
-	int c;
-	int permissions = 0775; // permissions de base
-	while((c = getopt(argc, argv, "mv")) != -1) {
-		switch(c) {
-			case 'm':
-				permissions = kmkdir_m(argc, argv);
-				break;
-			case 'v': 
-				kmkdir_v(argv[argc-1]);
-				break;
-			case '?': // option pas reconnu
-				exit(-1);	
-		}
-	}
-	mkdir(argv[argc-1], permissions);
-	return 0;
+int kmkdir(int argc, char * argv[]) {
+    if (argc<2) {
+        printf("kmkdir : missing operand\n");
+        return -1;
+    }
+    int c;
+    int permissions = 0775; // permissions de base
+    while((c = getopt(argc, argv, "mv")) != -1) {
+        switch(c) {
+            case 'm':
+                permissions = kmkdir_m(argc, argv);
+                break;
+            case 'v':
+                kmkdir_v(argv[argc-1]);
+                break;
+            case '?': // option pas reconnu
+                exit(-1);
+        }
+    }
+    mkdir(argv[argc-1], permissions);
+    return 0;
+}
+
+/**
+ * Init
+ *
+ * S'enregistre dans le shell dans le cas d'un chargement de la librairie
+ * dynamique
+ */
+void Init(EnregisterCommande enregisterCommande) {
+    enregisterCommande("mkdir", kmkdir);
 }
--- src/libs/mkdir/mkdir.h
+++ src/libs/mkdir/mkdir.h
@@ -1,9 +1,6 @@
 #pragma once
 
-int octal_decimal(int n);
-
-int kmkdir_v(char * dirname);
-
-int kmkdir_m(int argc, char * const argv[]);
+#include "../../LIB.h"
 
 int kmkdir(int argc, char * const argv[]);
+int Init(EnregisterCommande);
