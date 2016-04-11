76dd1786e990b9c3d664a25d78a6c16e9a3c9789
Quentin Billaud <quentin.billaud@etu.univ-nantes.fr>
2016-04-11 16:25:37 +0200

    chmod récursif qui marche

--- src/libs/chmod/chmod.c
+++ src/libs/chmod/chmod.c
@@ -1,11 +1,24 @@
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <libgen.h>
 #include <math.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include "../../LIB.h"
+#include "../../DEBUG.h"
+
+//flags
+const int F_FORCE = 1;      //-f
+const int F_REC   = 1<<1;   //-r, -R
+const int F_VERB  = 1<<2;   //-v
 
 int octal_decimal(int n)
 {
@@ -20,26 +33,150 @@ int octal_decimal(int n)
     return decimal;
 }
 
-int chmodLib(int argc, char *argv[]) {
-    if (argc>1)
-    {
-        int droit = atoi(argv[1]);
-        droit = octal_decimal(droit);
-        int resultat = chmod(argv[2],(mode_t)droit);
-        if (resultat==-1)
-           {
-               printf("Erreur\n");
-               return -1;
-           }
-        return 0;
-    }
-    return -1;
+/**
+ * usage
+ */
+void usage() {
+    printf("\
+Knut chmod\n\n\
+usage: rm [0-7] [-Rfv] file ...\n\
+");
 }
 
-int chmodLib_R(int argc, char *argv[]){
-    //TODO
+void chmodElem(char *path, int options, int perms);
+
+void chmodDirContent(char *path, int options, int perms){
+    DIR *dirp_src;
+    struct dirent *dptr_src;
+
+    if ((dirp_src = opendir(path)) == NULL) {
+        fprintf(stderr, "Can't open directory %s : %s\n",
+                path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    while ((dptr_src = readdir(dirp_src)) != NULL) {
+        DEBUG("%s/%s", path, dptr_src->d_name);
+
+        if ((strcmp(dptr_src->d_name, "..") != 0)
+                && (strcmp(dptr_src->d_name, ".") != 0)) {
+
+            char child_path[strlen(path) + strlen(dptr_src->d_name) + 2];
+            strcpy(child_path, path);
+            strcat(child_path, "/");
+            strcat(child_path, dptr_src->d_name);
+
+            chmodElem(child_path, options, perms); //on supprime l'élément
+        }
+    }
+
+    if (closedir(dirp_src) != 0) {
+        perror("Closedir error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void chmodElem(char *path, int options, int perms){
+    struct stat st;
+    if (stat(path, &st) == -1) {
+        if (errno == ENOENT) {
+            perror("No such file");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //si pas de -f, on vérifie que l'élement est accessible en écriture avant
+    //de modifier ses droits
+    char ok = 'n';
+    if ((options & F_FORCE) != F_FORCE) {
+        DEBUG("No force");
+        if (access(path, W_OK) != 0) {
+            printf("override permissions for %s ? [y/n] ", path);
+            ok = getchar();
+            printf("\n");
+            fseek(stdin, 0L, SEEK_END);
+            DEBUG("getchar : %c", ok);
+        } else {
+            ok = 'y';
+        }
+    } else {
+        ok = 'y';
+    }
+
+    if (ok == 'y') {
+
+        if ((options & F_VERB) == F_VERB) {
+            printf("Changing file %s permissions\n", path);
+        }
+
+        //si on est sur un dossier, on regarde si on a l'option -r
+        if (S_ISDIR(st.st_mode)) {
+            if ((options & F_REC) == F_REC) {
+                DEBUG("Delete %s content", path);
+                chmodDirContent(path, options, perms); //Modicfication des droits du contenu
+
+                DEBUG("chmod %s", path);
+                if (chmod(path,(mode_t)perms) == -1) {
+                    fprintf(stderr, "Can't modify permissions %s : %s\n", path, strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else{
+                DEBUG("chmod %s",path);
+                if (chmod(path,(mode_t)perms) == -1) {
+                    fprintf(stderr, "Can't modify %s : %s\n", path, strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
+            }
+        } else { //on est sur un fichier
+            DEBUG("chmod %s", path);
+            if (chmod(path,(mode_t)perms) == -1) {
+                fprintf(stderr, "Can't modify %s : %s\n", path, strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+        }
+
+    }
+
+}
+
+int chmodLib(int argc, char *argv[]) {
+    int options = 0;
+    //Conversion des droits
+    //marche seulement quand les options sont après les droits
+    int perms = atoi(argv[1]);
+    perms = octal_decimal(perms);
+
+    char c;
+    while((c = getopt(argc, argv, "Rvf")) != -1) {
+        switch(c) {
+            case 'R': //on veut modifier en récursif
+                DEBUG("-R option");
+                options |= F_REC;
+                break;
+            case 'f':
+                DEBUG("-f option");
+                options |= F_FORCE;
+                break;
+            case 'v':
+                DEBUG("-v option");
+                options |= F_VERB;
+                break;
+            case '?': //option non reconnue
+            default:
+                usage();
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (argc < 2) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+    chmodElem(argv[argc-1], options, perms);
+
     return 0;
 }
+
 
 /**
  * Init
