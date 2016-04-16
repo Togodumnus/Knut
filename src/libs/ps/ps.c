d13525e380758d9ead03f98ec4d3769b2d3a7c60
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-16 15:47:00 +0200

    Work on ps but not a big deal

--- src/libs/ps/ps.c
+++ src/libs/ps/ps.c
@@ -1,16 +1,18 @@
+#define _BSD_SOURCE
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <getopt.h>
 #include <string.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdlib.h>
-
 #include <sys/stat.h>
 #include <unistd.h>
 #include <pwd.h>
 #include <ctype.h>
 #include <stdbool.h>
 
+#include "../../DEBUG.h"
 #include "../../LIB.h"
 
 /*TODO
@@ -18,217 +20,261 @@
 Lien important :
 https://www.kernel.org/doc/Documentation/filesystems/proc.txt
 
-TROUVER POURQUOI CERTAINS UID N'ONT PAS DE NOMS D'UTILISATEURS
-
 TROUVER %CPU %MEM TTY
 
 Calcul CPU
 http://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat
 */
 
+const int SLEEP_TIME = 1000000; //10ms
+
 char* REPERTOIRE_PROC = "/proc";
 
-typedef struct{
-    char* user;
-    int pid;
-    int vsz;
-    int rss;
-}proc;
+/**
+ * proc
+ *
+ * Représente un processus
+ */
+typedef struct proc{
+    char*  user;
+    int    pid;
+    int    vsz;
+    int    rss;
+    float cpu;
+} proc;
 
-void infos_from_stat(char* path, proc* process) {
+/**
+ * systemStat
+ *
+ * Quelques infos du système
+ */
+typedef struct systemStat {
+    long unsigned int   totalCpuTime;
+    long                clock;          //hertz
+} systemStat;
 
-    /*FILE* statusf;
-    char c;
-    char *infoStat = "\0";
-    char **tabChar = NULL;
-    int posTabChar = 0;
-    int lenInfoStat = 0;
 
-    statusf = fopen(path, "r");
-    if(!statusf){
+/**
+ * split
+ *
+ * @param  {char *}     str
+ * @param  {char *}     delim
+ * @param  {int *}      nb      nombre de string lues
+ *
+ * @return {char **}    Tableau de strings (à free à la fin)
+ */
+char **split(char *str, char *delim, int *nb) { //pas utilisée
+
+    char **res = (char **) malloc(sizeof(char *));
+    int n = 0;
+
+    char *p = strtok(str, delim);
+    while (p) {
+
+        res[n] = p;
+        n++;
+        res = (char **) realloc(res, (n + 1) * sizeof(char *));
+
+        if (res == NULL) {
+            perror("realloc error");
+            exit(EXIT_FAILURE);
+        }
+
+        p = strtok(NULL, delim);
+    }
+
+    if (nb != NULL) {
+        *nb = n;
+    }
+
+    return res;
+}
+
+/**
+ * getSystemStat
+ *
+ * Lecture de quelques paramètres système en début de ps
+ *
+ * @param  {systemStat *}   sysStat     Où sauver les données
+ */
+void getSystemStat(systemStat *sysStat) { //pas utilisée
+    sysStat->clock = sysconf(_SC_CLK_TCK);
+
+    FILE *statf = fopen("/proc/stat", "r");
+    if(!statf){
+        perror("Can't open status");
         exit(EXIT_FAILURE);
     }
 
-    do{
-        c = fgetc(statusf);
-        if (isspace(c)){
-            tabChar[posTabChar] = infoStat;
-            posTabChar++;
-            infoStat = '\0';
-        }else{
-            lenInfoStat = strlen(infoStat);
-            infoStat[lenInfoStat] = c;
-            infoStat[lenInfoStat+1] = '\0';
-        }
+    char *line = NULL;
+    size_t n = NULL;
 
-        printf("%c\n",c);
-    }while (!feof(statusf));
+    if (getline(&line, &n, statf) == -1) {
+        perror("Can't read file");
+        exit(EXIT_FAILURE);
+    }
 
+    int nb = 0;
+    char **items = split(line, " ", &nb);
 
-    fclose(statusf);*/
+    sysStat->totalCpuTime = 0;
+    for (int i = 1; i < 5; i++) {
+        sysStat->totalCpuTime += atoi(items[i]);
+    }
+
+    free(items);
+    free(line);
+
+    fclose(statf);
 }
 
+/**
+ * infos_from_status
+ *
+ * Parsing du fichier status correspondant à un processus
+ *
+ * @param  {char *} path    Le chemin vers le fichier status
+ * @param  {proc *} process La structure à remplir
+ */
 void infos_from_status(char* path, proc* process) {
-    char line[100], *p;
-    int tmp = 0, pid=0 ,uid =0, vsz = 0 , rss = 0;
-    struct passwd *user;
-    bool vmSizeHere = false, vmRssHere = false;
+
+    char line[100];
 
     FILE* statusf;
 
     statusf = fopen(path, "r");
     if(!statusf){
+        perror("Can't open status");
         exit(EXIT_FAILURE);
     }
 
-    while(fgets(line, 100, statusf)) {
-        tmp = 0;
-        //On est dans la ligne Uid :
+    while (fgets(line, 100, statusf)) {
+        char *token;
+
+        //Uid
         if(strncmp(line, "Uid:", 4) == 0){
-            //On récupère l'uid de la ligne et on le transforme en int dans la variable uid
-            p = line + strlen("Uid:");
-            while(isspace(*p)) {
-                ++p;
-            }
-            for (int i = 0; i < 5; ++i)
-            {
-                if (isdigit(p[i])){
-                    tmp = p[i] - '0';
-                    uid = 10 * uid + tmp;
-                }
-            }
+            token = strtok(line, "\t"); //"Uid:"
+            token = strtok(NULL, "\t"); //l'uid
+
+            int uid = atoi(token);
+            struct passwd *user;
 
             //A partir de l'uid, on obtient le nom de l'utilisateur
-            if (!(user = getpwuid(uid))){ // user name
+            if (!(user = getpwuid(uid))){
                 process->user = "?";
-            }else{
+            } else {
                 process->user = user->pw_name;
             }
         }
 
         //On est dans la ligne Pid :
         else if(strncmp(line, "Pid:", 4) == 0){
-
-            //On récupère le pid qu'on transforme en int
-            p = line + strlen("Pid:");
-            while(isspace(*p)) {
-                ++p;
-            }
-            for (int i = 0; i < 5; ++i)
-            {
-                if (isdigit(p[i])){
-                    tmp = p[i] - '0';
-                    pid = 10 * pid + tmp;
-                }
-            }
+            token = strtok(line, "\t"); //"Pid:"
+            token = strtok(NULL, "\t"); //le pid
 
             //on stocke dans la structure proc le pid
-            process->pid = pid;
+            process->pid = atoi(token);
         }
 
         //On est dans la ligne VmSize
         else if(strncmp(line, "VmSize:", 4) == 0){
-            vmSizeHere = true;
-            //On récupère le pid qu'on transforme en int
-            p = line + strlen("VmSize:");
-            while(isspace(*p)) {
-                ++p;
-            }
-            for (int i = 0; i < 12; ++i)
-            {
-                if (isdigit(p[i])){
-                    tmp = p[i] - '0';
-                    vsz = 10 * vsz + tmp;
-                }
-            }
+            token = strtok(line, "\t"); //"VmSize:"
+            token = strtok(NULL, "\t");
 
             //on stocke dans la structure proc le VSZ
-            process->vsz = vsz;
+            process->vsz = atoi(token);
         }
         //On est dans la ligne VmRSS
         else if(strncmp(line, "VmRSS:", 4) == 0){
-            vmRssHere = true;
-            //On récupère le pid qu'on transforme en int
-            p = line + strlen("VmRSS:");
-            while(isspace(*p)) {
-                ++p;
-            }
-            for (int i = 0; i < 12; ++i)
-            {
-                if (isdigit(p[i])){
-                    tmp = p[i] - '0';
-                    rss = 10 * rss + tmp;
-                }
-            }
+            token = strtok(line, "\t"); //"VmSize:"
+            token = strtok(NULL, "\t");
 
             //on stocke dans la structure proc le VSZ
-            process->rss = rss;
+            process->rss = atoi(token);
         }
 
-    }
-    //Si la ligne vmSize n'existe pas dans le status
-    if (!vmSizeHere){
-        process->vsz = 0;
-    }
-    if (!vmRssHere){
-        process->rss = 0;
     }
 
     fclose(statusf);
 }
 
 
+/**
+ * ps
+ *
+ * Affichage des processus
+ *
+ * @param  {int}    options
+ */
+void ps(int options) {
 
-int psNoOpt(int argc, char *argv[]){
     DIR *rep;
-    DIR *sousRep;
+    DIR *processRep;
 
-    struct dirent* fichierLu = NULL; /* Déclaration d'un pointeur vers la structure dirent. */
+    struct dirent* fichierLu = NULL;
     struct dirent* fichierLuBis = NULL;
 
     if ((rep=opendir(REPERTOIRE_PROC))==NULL) {
         printf("Not opened repository\n");
         exit(EXIT_FAILURE);
     }
-    printf("repository /proc opened successfully\n");
+    DEBUG("repository /proc opened successfully");
 
-    printf ("USER         PID  %cCPU  %cMEM    VSZ      RSS TTY      STAT START   TIME COMMAND\n",'%','%');
+    /*systemStat sysStat = {0};*/
+    /*getSystemStat(&sysStat);*/
+
+    printf("USER\tPID\t%%CPU\t%%MEM\tVSZ\tRSS");
+    printf("TTY\tSTAT\tSTART\tTIME\tCOMMAND\n");
 
     while ((fichierLu = readdir(rep)) != NULL){
-        if (fichierLu->d_type == 4 && atoi(fichierLu->d_name)!=0){ // Si c'est un dossier correspondant aux processus
+        //Si c'est un dossier correspondant aux processus
+        if (fichierLu->d_type == 4 && atoi(fichierLu->d_name) != 0){
 
-            // On concatène les chemins pour ouvrir le bon dossier correpsondant
-            char* concat_proc_nameOfFile = (char *) malloc(2+strlen(REPERTOIRE_PROC)+strlen(fichierLu->d_name));
-            strcpy(concat_proc_nameOfFile, REPERTOIRE_PROC);
-            strcat(concat_proc_nameOfFile, "/");
-            strcat(concat_proc_nameOfFile, fichierLu->d_name);
+            //On concatène les chemins pour ouvrir le bon dossier correspondant
+            char processDir[
+                2 + strlen(REPERTOIRE_PROC) + strlen(fichierLu->d_name)
+            ];
+            strcpy(processDir, REPERTOIRE_PROC);
+            strcat(processDir, "/");
+            strcat(processDir, fichierLu->d_name);
 
-            if ((sousRep=opendir(concat_proc_nameOfFile))==NULL) {
+            //on ouvre un processus
+            if ((processRep = opendir(processDir)) == NULL) {
                 printf("Not closed repository\n");
                 exit(EXIT_FAILURE);
-            }else{
-                //On a réussi à ouvrir le dossier correspondant au processus
-                while ((fichierLuBis = readdir(sousRep)) != NULL){
-                    proc process;
-                    if (strcmp(fichierLuBis->d_name,"status") == 0){
-                        char* path_to_status = (char *) malloc(1+strlen(concat_proc_nameOfFile)+strlen("/status"));
-                        strcpy(path_to_status, concat_proc_nameOfFile);
+            } else { //On a réussi à ouvrir le dossier correspondant au process
+
+                while ((fichierLuBis = readdir(processRep)) != NULL) {
+
+                    proc process = {0};
+
+                    if (strcmp(fichierLuBis->d_name, "status") == 0) {
+                        char path_to_status[
+                            1 + strlen(processDir)
+                            + strlen("/status")
+                        ];
+                        strcpy(path_to_status, processDir);
                         strcat(path_to_status, "/status");
 
-                        infos_from_status(path_to_status,&process);
-                        printf("%10s %5d %20d %7d \n",
-                                process.user,
-                                process.pid,
-                                process.vsz,
-                                process.rss);
-                    }
-                    /*else if (strcmp(fichierLuBis->d_name,"stat") == 0){ 
-                        char* path_to_stat = (char *) malloc(1+strlen(concat_proc_nameOfFile)+strlen("/stat"));
-                        strcpy(path_to_stat, concat_proc_nameOfFile);
+                        infos_from_status(path_to_status, &process);
+
+                        printf(
+                            "%10s %5d %20d %7d \n",
+                            process.user,
+                            process.pid,
+                            process.vsz,
+                            process.rss
+                        );
+                    } else if (strcmp(fichierLuBis->d_name, "stat") == 0){
+                        char path_to_stat[
+                            1 + strlen(processDir)
+                            + strlen("/stat")
+                        ];
+                        strcpy(path_to_stat, processDir);
                         strcat(path_to_stat, "/stat");
 
-                        infos_from_stat(path_to_stat,&process);
-                    }*/
+                        infos_from_stat(path_to_stat, &process, &sysStat);
+                    }
 
                 }
             }
@@ -236,28 +282,32 @@ int psNoOpt(int argc, char *argv[]){
     }
 
     if (closedir(rep) == -1){ /* S'il y a eu un souci avec la fermeture */
+        perror("Can't close dir");
         exit(EXIT_FAILURE);
     }
-    printf("Dossier /proc a ete ferme avec succes\n");
 
-
-    return 0;
+    DEBUG("Dossier /proc a ete ferme avec succes");
 
 }
 
 int kPs(int argc,char *argv[]){
+    int options = 0;
+
     int opt = 0;
-    while((opt=getopt(argc, argv, "ef"))!=-1)
-    switch (opt)
-    {
-        case 'e':
-            //return psLib_e(argc,argv);
-            break;
-        case 'f':
-            //return psLib_f(argc, argv);
-            break;
+    while ((opt=getopt(argc, argv, "ef"))!=-1) {
+        switch (opt) {
+            case 'e':
+                //TODO
+                break;
+            case 'f':
+                //TODO
+                break;
+        }
     }
-    return psNoOpt(argc,argv);
+
+    ps(options);
+
+    return 0;
 }
 
 /**
